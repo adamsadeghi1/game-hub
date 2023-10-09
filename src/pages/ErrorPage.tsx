91b30bdbde59db4d1e6eda1afbe576974c1aba52
Adam <adam.sadeghi1@gmail.com>
2023-10-09 14:08:00 -0700

    Handle global errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,25 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <>
+      <NavBar />
+      <Box paddingLeft={5}>
+        <Heading as="h1" fontSize="6xl" marginBottom={4}>
+          Oops
+        </Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "Invalid address."
+            : "Unexpected error happened."}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
