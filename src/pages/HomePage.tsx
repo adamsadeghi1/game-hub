b918b6f9003edd518465c2ad5fb67eda307f9a84
Adam <adam.sadeghi1@gmail.com>
2023-10-09 13:57:54 -0700

    add route to app

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,22 @@
 import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PageHeading from "./components/PageHeading";
-import PlatformList from "./components/PlatformList";
-import SortSelector from "./components/SortSelector";
+import GameGrid from "../components/GameGrid";
+import GenreList from "../components/GenreList";
+import PageHeading from "../components/PageHeading";
+import PlatformList from "../components/PlatformList";
+import SortSelector from "../components/SortSelector";
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       gridTemplate={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       gridTemplateColumns={{
         base: `1fr`,
         lg: `200px 1fr`,
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <Show above="lg">
         <GridItem area="aside" padding={5}>
           <GenreList />
@@ -39,6 +35,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
